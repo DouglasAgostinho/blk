fa47c9d7104bf26b2ed52b061300058e4995222e
DouglasAgostinho <marques.aut@gmail.com>
2022-08-12 20:02:27 -0300

    add new variables mem_pool & blk_tx

--- transactions.py
+++ transactions.py
@@ -2,8 +2,17 @@
 
 #-------------------- variables --------------------
 
+
+mem_pool = [{"send_add": "Sender Address", "recv_add": "Receiver Address", 
+            "amount": "Amount Tx", "time_stamp": "time stamp",
+            "tx_sig": "transaction signature"}]
+
+blk_tx = {"send_add": "Sender Address", "recv_add": "Receiver Address", 
+            "amount": "Amount Tx", "time_stamp": "time stamp",
+            "tx_sig": "transaction signature"}
+
 #-------------------- main --------------------
 
 if __name__ == "__main__":
-    print('just testing for now')
+    print("\n just testing for now \n")
     
