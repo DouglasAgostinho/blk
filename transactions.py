5d734a7b978e30b1e14f80ca67b90315bd0442f7
DouglasAgostinho <marques.aut@gmail.com>
2022-08-13 14:21:26 -0300

    add new class method new_tx

--- transactions.py
+++ transactions.py
@@ -1,21 +1,38 @@
 #-------------------- imports --------------------
-
+from hashlib import new
+import datetime
 import tools
 
 #-------------------- variables --------------------
 
-
-mem_pool = [{"send_add": "Sender Address", "recv_add": "Receiver Address", 
-            "amount": "Amount Tx", "time_stamp": "Time Stamp",
-            "tx_id": "Transaction ID", "tx_sig": "Transaction Signature"}]
-
-blk_tx = {"send_add": "Sender Address", "recv_add": "Receiver Address", 
-            "amount": "Amount Tx", "time_stamp": "Time Stamp",
-            "tx_id": "Transaction ID", "tx_sig": "Transaction Signature"}
+#block tx model
+#
+# blk_tx = {"send_add": "Sender Address", "recv_add": "Receiver Address", 
+#             "amount": "Amount Tx", "time_stamp": "Time Stamp",
+#             "tx_id": "Transaction ID", "tx_sig": "Transaction Signature"}
 
 
 #-------------------- class --------------------
-    
+
+class Transaction:
+        
+    def __init__(self, send_add, recv_add, amount):
+        self.send_add = send_add
+        self.recv_add = recv_add
+        self.amount = amount
+        self.time = str(datetime.datetime.now())
+
+    def new_tx(self):
+        initial_tx = {"send_add": self.send_add, "recv_add": self.recv_add, 
+            "amount": self.amount, "time_stamp": self.time}
+        
+        tx_id = tools.hash(initial_tx)
+
+        new_tx = {"send_add": self.send_add, "recv_add": self.recv_add, 
+            "amount": self.amount, "time_stamp": self.time, "tx_id": tx_id}
+        
+        tools.log("[Transactions -> new_tx] - New transaction created")
+        return(new_tx)
 
 #-------------------- main --------------------
 
@@ -24,9 +41,13 @@ def test_main():
 
     print("\n Test routine started \n")
 
-    test_msg = 123 
+    
 
-    print(tools.hash(test_msg))
+    for i in range(3):
+        tx = Transaction(i*10, i*20, i)
+        print(tx.new_tx())
+
+    
 
     print("\n Test routine ended \n")
 
