4cc80deb05676391263eee8c942d8a6755ea5607
DouglasAgostinho <marques.aut@gmail.com>
2022-08-13 09:01:39 -0300

    init hash tests

--- transactions.py
+++ transactions.py
@@ -1,5 +1,7 @@
 #-------------------- imports --------------------
 
+import tools
+
 #-------------------- variables --------------------
 
 
@@ -11,16 +13,25 @@ blk_tx = {"send_add": "Sender Address", "recv_add": "Receiver Address",
             "amount": "Amount Tx", "time_stamp": "Time Stamp",
             "tx_id": "Transaction ID", "tx_sig": "Transaction Signature"}
 
+
+#-------------------- class --------------------
+    
+
 #-------------------- main --------------------
 
 def test_main():
+    #function used for test or Debug purposes
+
     print("\n Test routine started \n")
 
+    test_msg = 123 
 
+    print(tools.hash(test_msg))
 
     print("\n Test routine ended \n")
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
+
     #print("\n just testing for now \n")
     test_main()
     
