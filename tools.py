d399b5896c1e8eee19ef77d5d15ca919af35b093
DouglasAgostinho <marques.aut@gmail.com>
2022-08-13 09:01:21 -0300

    add hash function

--- tools.py
+++ tools.py
@@ -1,10 +1,26 @@
 #-------------------- imports --------------------
 
+import hashlib
+
 
 #-------------------- classes --------------------
 
 
+#-------------------- functions --------------------
 
+def hash(msg):
+    #handle data to hash, verify message type and return hash value
+    
+    if type(msg) == str:
+        print("string")
+        msg = str.encode(msg)
+    elif type(msg) == int:
+        print("int")
+        msg = msg.to_bytes(4, byteorder ="big")
+    else:
+        print("\n Please check your message. \n")
+
+    return(hashlib.sha256(msg).hexdigest())
 
 
 #-------------------- main --------------------
