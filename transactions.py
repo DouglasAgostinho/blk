a17df4c57ab229c816bc60a0c226005070968e7a
DouglasAgostinho <marques.aut@gmail.com>
2022-08-13 08:21:01 -0300

    Add main test function and update TX variables

--- transactions.py
+++ transactions.py
@@ -4,15 +4,23 @@
 
 
 mem_pool = [{"send_add": "Sender Address", "recv_add": "Receiver Address", 
-            "amount": "Amount Tx", "time_stamp": "time stamp",
-            "tx_sig": "transaction signature"}]
+            "amount": "Amount Tx", "time_stamp": "Time Stamp",
+            "tx_id": "Transaction ID", "tx_sig": "Transaction Signature"}]
 
 blk_tx = {"send_add": "Sender Address", "recv_add": "Receiver Address", 
-            "amount": "Amount Tx", "time_stamp": "time stamp",
-            "tx_sig": "transaction signature"}
+            "amount": "Amount Tx", "time_stamp": "Time Stamp",
+            "tx_id": "Transaction ID", "tx_sig": "Transaction Signature"}
 
 #-------------------- main --------------------
 
+def test_main():
+    print("\n Test routine started \n")
+
+
+
+    print("\n Test routine ended \n")
+
 if __name__ == "__main__":
-    print("\n just testing for now \n")
+    #print("\n just testing for now \n")
+    test_main()
     
