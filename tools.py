c5d42c3f7f89df907f19e0a7ea4f5a441fd2f794
DouglasAgostinho <marques.aut@gmail.com>
2022-08-13 00:21:34 -0300

    updated tools with test function

---
+++ tools.py
@@ -0,0 +1,21 @@
+#-------------------- imports --------------------
+
+
+#-------------------- classes --------------------
+
+
+
+
+
+#-------------------- main --------------------
+
+def test_main():
+    #function created for test purposes
+    pass
+
+
+if __name__ == "__main__":
+
+    print("\n This is a module, should be imported not run \n")
+
+    test_main()
