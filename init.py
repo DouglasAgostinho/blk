22113e00e17aa1816db3f6829f45baa6d5e9050b
DouglasAgostinho <marques.aut@gmail.com>
2022-10-18 20:20:08 -0300

    Added comments on Init logic

--- init.py
+++ init.py
@@ -1,10 +1,21 @@
+#------------------------------ imports ------------------------------
 import block
+import platform
 
 #------------------------------ constants ------------------------------
-PATH = r"C:\Users\iceli\OneDrive\Documentos\python\blk\data\\"
+
+# get the current running os to determine with PATH to follow
+OS_NAME = platform.system()
+
+if OS_NAME.lower() == "linux":
+    PATH = r"/home/ice/Documents/python/blk/data/"
+else:
+    PATH = r"C:\Users\iceli\OneDrive\Documentos\python\blk\data\\"
+
+#------------------------------ Functions and Classes definition ------------------------------
 
 def intialization ():
-        
+    # Initialization function        
     blocks = block.block_find(PATH)
 
     print(blocks)
